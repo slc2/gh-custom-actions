6656a219865152bdbcf9908596311bc0f8ea7d39
slc2 <sbspfun@yahoo.com>
2025-07-26 10:43:49 -0400

    example output from javascript action

--- actions/deploy-s3-javascript/main.js
+++ actions/deploy-s3-javascript/main.js
@@ -7,7 +7,9 @@ function run() {
     const bucket_name = core.getInput('bucket-name', { required: true}); // Dummy input for now
 
     core.notice('Hello from custom javascript action!');
-    core.info(`Event name: ${bucket_name}`);
+    core.info(`Bucket name: ${bucket_name}`);
+
+    core.setOutput('website-url', `https://${bucket_name}.restofurlhere.com`);
 }
 
 run();
